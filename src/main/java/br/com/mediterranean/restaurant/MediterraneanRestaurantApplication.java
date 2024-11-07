b9ccbd54ae9f84a57ec4afb82e0de22abdd4d184
carlosm-dev <carlosaraujodemedeiros@gmail.com>
2024-11-07 10:02:11 -0300

    init

---
+++ src/main/java/br/com/mediterranean/restaurant/MediterraneanRestaurantApplication.java
@@ -0,0 +1,13 @@
+package br.com.mediterranean.restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediterraneanRestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediterraneanRestaurantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/mediterranean/restaurant/MediterraneanRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mediterranean.restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediterraneanRestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
