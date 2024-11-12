55768913245571dc086015e3ce352c8baf1bb366
carlosm-dev <carlosaraujodemedeiros@gmail.com>
2024-11-11 21:45:15 -0300

    Criando entidades

---
+++ src/main/java/br/com/mediterranean/restaurant/domain/model/Restaurante.java
@@ -0,0 +1,34 @@
+package br.com.mediterranean.restaurant.domain.model;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Restaurante {
+	
+	@Id
+	@EqualsAndHashCode.Include
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String nome;
+	
+	@Column(name="taxa_frete", nullable = false)
+	private BigDecimal taxaFrete;
+	
+	@ManyToOne // Muitos restaurantes possuem uma unica coizinha
+	@JoinColumn(name="cozinha_id", nullable = false)
+	private Cozinha cozinha;
+}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+insert into cozinha(nome) values('Tailandesa')
+insert into cozinha(nome) values('Indiana')
+
+insert into restaurante (nome, taxa_frete, cozinha_id) values ('Thai Gourmet',10,1)
